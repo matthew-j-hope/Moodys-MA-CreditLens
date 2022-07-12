02ff2e3b7ea41934be0c74eae0cd2b7b53adff26
matthew-j-hope <matthew.j.hope@gmail.com>
2022-07-12 12:00:51 +0000

    Pushing data

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Moodys-MA-CreditLens
+# IMPORTANT
+# This files included in this repo are invalid, the credentials listed are invalid and bear no relation to any previous passwords or users.
+# The payload data for the given URLS is also invalid and in no way relates to how the endpoint actually operates.
+# This is here purely to test detections for potential credentials leaks on public github repos.
---
+++ login.py
@@ -0,0 +1,20 @@
+import requests
+
+#IMPORTANT
+# This file is invalid, the credentials listed below are invalid and bear no relation to any previous passwords or users.
+# The payload data for the given URLS is also invalid and in no way relates to how the endpoint actually operates.
+# This is here purely to test detections for potential credentials leaks on public github repos.
+
+POST-LOGIN-URL = 'https://riskcalcplus.moodysanalytics.com/App/data'
+
+REQUEST-URL = 'https://login.moodysanalytics.com/'
+
+payload = {
+    'username': 'admin',
+    'pass': 'roottest123'
+}
+
+with requests.Session() as session:
+    post = session.post(POST-LOGIN-URL, data=payload)
+    r = session.get(REQUEST-URL)
+    print(r.text)
