0b2c1479c8ef1252f1f75367e6be2055bcce88db
matthew-j-hope <37869002+matthew-j-hope@users.noreply.github.com>
2023-01-18 17:11:26 +0000

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 import requests
 
-#IMPORTANT
+# IMPORTANT
 # This file is invalid, the credentials listed below are invalid and bear no relation to any previous passwords or users.
 # The payload data for the given URLS is also invalid and in no way relates to how the endpoint actually operates.
 # This is here purely to test detections for potential credentials leaks on public github repos.
